5bb293e25d90c33dcba0a2bbfd2d7b5dc1003d32
Ahmad Madni <snankls@gmail.com>
2025-12-02 01:02:55 +0500

    update icon box 2

--- wp-content/plugins/themerange-plugin/elementor/elements/icon_box.php
+++ wp-content/plugins/themerange-plugin/elementor/elements/icon_box.php
@@ -222,22 +222,21 @@ class TR_Elementor_Widget_Icon_Box extends TR_Elementor_Widget_Base{
 			<section class="policy_section bg-light">
 				<div class="container">
 					<div class="row">
-
 						<?php foreach($settings['icon_box'] as $index => $item) : ?>
-                        <div class="col-lg-4">
-                            <div class="iconbox_block layout_icon_left">
-                                <div class="iconbox_icon tr-icon">
-                                	<?php \Elementor\Icons_Manager::render_icon($item['icon']); ?>
-                                </div>
-                                <div class="iconbox_content">
-                                    <h3 class="iconbox_title"><?php echo wp_kses($item['title'], $allowed_html); ?></h3>
-                                    <p class="mb-0">
-                                        <?php echo wp_kses($item['text'], $allowed_html); ?>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    	<?php endforeach; ?>
+						<div class="col-lg-4">
+							<div class="iconbox_block">
+								<div class="iconbox_icon bg-warning-subtle tr-icon">
+									<?php \Elementor\Icons_Manager::render_icon($item['icon']); ?>
+								</div>
+								<div class="iconbox_content">
+									<h3 class="iconbox_title"><?php echo wp_kses($item['title'], $allowed_html); ?></h3>
+									<p class="mb-0">
+										<?php echo wp_kses($item['text'], $allowed_html); ?>
+									</p>
+								</div>
+							</div>
+						</div>
+						<?php endforeach; ?>
 					</div>
 				</div>
 			</section>
