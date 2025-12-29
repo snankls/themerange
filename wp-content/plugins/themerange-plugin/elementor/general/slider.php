7a59b6bb0f27e91af7372357798cd9c45881830a
Ahmad Madni <155092937+snankls@users.noreply.github.com>
2025-12-29 19:37:12 +0500

    slider update

--- wp-content/plugins/themerange-plugin/elementor/general/slider.php
+++ wp-content/plugins/themerange-plugin/elementor/general/slider.php
@@ -128,107 +128,140 @@ class TR_Elementor_Widget_Slider extends TR_Elementor_Widget_Base{
 		$allowed_html = tr_allowed_html();
 	?>
     
-		<!-- IT Solution Hero Section - Start
-    ================================================== -->
-    <section class="it_solution_hero_section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-7">
-            <div class="it_solution_hero_content" style="background-image: url('assets/images/shapes/it_solution_hero_bg_1.svg');">
-              
-              <div class="heading_focus_text mb-0 d-inline-flex align-items-center">
-                👋 Welcome To <span class="badge bg-secondary text-white">Our Agency</span>
-              </div>
-              <h1>We Build Modern Websites That Grow Your Business</h1>
-              <p>In today’s competitive market, every business needs efficient IT solutions, modern websites, and high-performance mobile apps to grow faster, work smarter, and stay ahead of the competition.</p>
-
-              <ul class="btns_group unordered_list p-0 justify-content-start">
-                <li>
-                  <a class="btn" href="pricing.html">
-                    <span class="btn_label" data-text="Get Started">Get Started</span>
-                    <span class="btn_icon">
-                      <i class="fa-solid fa-arrow-up-right"></i>
-                    </span>
-                  </a>
-                </li>
-
-                <li>
-                  <a class="btn" href="pricing.html">
-                    <span class="btn_label" data-text="Get Quote">Get Quote</span>
-                    <span class="btn_icon">
-                      <i class="fa-solid fa-arrow-up-right"></i>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-
-            </div>
-          </div>
-
-          <div class="col-lg-5">
-            <ul class="it_solution_hero_images unordered_list">
-
-              <li>
-                <img src="assets/images/hero/it_solution_hero_image_1.webp" alt="Web & IT Services">
-              </li>
-
-              <li>
-                <div class="worldwide_clients">
-                  <div class="counter_value">150+</div>
-                  <p>
-                    Projects Delivered Across the Globe
-                  </p>
-                  <ul class="avatar_group unordered_list">
-                    <li><img src="assets/images/avatar/avatar_1.webp" alt="Client"></li>
-                    <li><img src="assets/images/avatar/avatar_2.webp" alt="Client"></li>
-                    <li><img src="assets/images/avatar/avatar_3.webp" alt="Client"></li>
-                    <li>5k+</li>
-                  </ul>
-                </div>
-              </li>
-
-              <li>
-                <ul class="categories unordered_list_block">
-                  <li>
-                    <a href="#!">
-                      <span>Web Development</span>
-                      <i class="fa-solid fa-plus"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!">
-                      <span>WordPress & Ecommerce</span>
-                      <i class="fa-solid fa-plus"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#!">
-                      <span>Digital Strategy</span>
-                      <i class="fa-solid fa-plus"></i>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-
-              <li>
-                <div class="business_growth_content" style="background-image: url('assets/images/hero/it_solution_hero_image_2.webp');">
-                  <div class="progress_content">
-                    <div class="business_growth" data-pie='{ "percent": 92 }'></div>
-                    <p>
-                      92% Client Satisfaction & Business Growth
-                    </p>
+    <!-- hero area start -->
+    <div class="section-triger pre-header">
+      <div class="tp-hero-area tp-hero-wd-spacing p-relative bg-position" data-background="assets/img/hero/hero-3/grid-bg.png">
+          <a href="#about" class="tp-smooth tp-hero-wd-btp d-none d-xl-block">
+            <svg class="upslide-1" width="11" height="25" viewBox="0 0 11 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10.4105 18.675L6.13362 24.72C6.06483 24.806 5.97087 24.8766 5.86024 24.9255C5.74961 24.9744 5.6258 25 5.5 25C5.37421 25 5.2504 24.9744 5.13977 24.9255C5.02914 24.8766 4.93518 24.806 4.86638 24.72L0.589462 18.675C0.511191 18.5572 0.483164 18.4215 0.509799 18.2892C0.536433 18.157 0.616215 18.0358 0.736567 17.9446C0.856918 17.8535 1.01099 17.7977 1.1745 17.7859C1.33801 17.7742 1.50165 17.8072 1.63963 17.8798L4.76665 19.633V0.598757C4.76665 0.439957 4.84391 0.287661 4.98144 0.175371C5.11897 0.0630817 5.3055 0 5.5 0C5.6945 0 5.88103 0.0630817 6.01856 0.175371C6.15609 0.287661 6.23336 0.439957 6.23336 0.598757V19.633L9.36038 17.8798C9.49835 17.8072 9.66199 17.7742 9.8255 17.7859C9.98901 17.7977 10.1431 17.8535 10.2634 17.9446C10.3838 18.0358 10.4636 18.157 10.4902 18.2892C10.5168 18.4215 10.4888 18.5572 10.4105 18.675Z" fill="#030303" />
+            </svg>
+          </a>
+          <div class="container-fluid containers container-1800">
+            <div class="row">
+                <div class="col-xl-2 col-lg-3">
+                  <div class="tp-hero-wd-text mb-30">
+                      <span class="d-inline-block mb-10">
+                        <svg width="110" height="12" viewBox="0 0 110 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect y="10" width="110" height="2" rx="1" fill="#030303" />
+                            <rect width="50" height="2" rx="1" fill="#999999" />
+                        </svg>
+                      </span>
+                      <p class="fw-400 fs-20"><span class="tp-text-common-black fw-500">We’re Aleric,</span> to build creative & user-centric design also develop for customer.</p>
                   </div>
                 </div>
-              </li>
-
-            </ul>
+                <div class="col-xxl-6 col-xl-7 col-lg-9">
+                  <div class="tp-hero-wd-title-wrap mb-30">
+                      <h2 class="tp-hero-wd-title tp-ff-teko fs-100 text-uppercase mb-20">Crafting 
+                      <span class="shape upslide d-inline-block">
+                        <svg width="112" height="60" viewBox="0 0 112 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 1C30.8503 1 34.8743 21.4865 34.8743 42.3801C46.3872 37.2924 68.8096 33.4936 66.3952 59C71.3134 46.6764 81.1497 11.5146 112 11.5146M40.5749 20.3333C41.1337 22.4815 42.1844 27.5918 41.9162 30.848C43.7046 29.039 48.4216 25.8281 52.982 27.4561" stroke="#030303" stroke-width="1.5" />
+                          </svg>
+                      </span><br>
+                        <span class="d-inline-block title-space">
+                              Web
+                            <span class="icons d-inline-block">
+                                  <svg width="40" height="16" viewBox="0 0 40 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M29.88 15.8569L39.552 9.01379C39.6896 8.90372 39.8026 8.75338 39.8808 8.57638C39.959 8.39937 40 8.20127 40 8C40 7.79873 39.959 7.60063 39.8808 7.42363C39.8026 7.24662 39.6896 7.09628 39.552 6.98621L29.88 0.143139C29.6915 0.0179054 29.4743 -0.0269382 29.2628 0.0156776C29.0512 0.0582934 28.8572 0.185945 28.7114 0.378507C28.5656 0.571069 28.4763 0.817589 28.4575 1.0792C28.4387 1.34081 28.4916 1.60264 28.6077 1.8234L31.4128 6.82663L0.958012 6.82663C0.70393 6.82663 0.460255 6.95026 0.280594 7.1703C0.100933 7.39035 0 7.6888 0 8C0 8.3112 0.100933 8.60965 0.280594 8.8297C0.460255 9.04975 0.70393 9.17337 0.958012 9.17337L31.4128 9.17337L28.6077 14.1766C28.4916 14.3974 28.4387 14.6592 28.4575 14.9208C28.4763 15.1824 28.5656 15.4289 28.7114 15.6215C28.8572 15.8141 29.0512 15.9417 29.2628 15.9843C29.4743 16.0269 29.6915 15.9821 29.88 15.8569Z" fill="#030303" />
+                                  </svg>
+                            </span>
+                            Design<br>
+                        </span>
+                        Solutions.</h2>
+                      <div class="tp-hero-wd-customer p-relative">
+                        <div class="tp-hero-customer d-flex align-items-center mb-30 mr-95">
+                            <img class="mr-20" src="assets/img/hero/avatar.png" alt="">
+                            <p class="fw-500 fs-16 tp-text-grey-1 lh-22 mb-0 d-inline-block">We have <b class="tp-text-common-black tp-ff-teko fs-22">24K+</b><br> 
+                              customers in world-wide.</p>
+                        </div>
+                        <div class="tp-hero-video d-flex align-items-center mb-30">
+                            <a class="tp-hero-video-btn popup-video mr-20" href="https://www.youtube.com/watch?v=go7QYaQR494">
+                              <span>
+                                  <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.2595 11.3877C15.9455 10.276 15.9455 7.79857 14.2595 6.68685L4.82854 0.468212C2.95139 -0.769576 0.455619 0.592952 0.476139 2.84432L0.588819 15.2073C0.609123 17.4355 3.08348 18.7571 4.94126 17.5321L14.2595 11.3877Z" fill="currentColor" />
+                                  </svg>
+                              </span>
+                            </a>
+                            <p class="lh-110-per mb-0 fw-500 fs-18 tp-text-grey-1">We’re Global <br> Brand Design Agency.</p>
+                        </div>
+                        <div class="tp-hero-wd-shape">
+                            <span class="shape-1 mb-5">
+                              <svg width="41" height="20" viewBox="0 0 41 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M20.6087 -1.90735e-06C9.34689 -1.90735e-06 0.217392 8.70558 0.217392 19.4445H41C41 8.70558 31.8705 -1.90735e-06 20.6087 -1.90735e-06Z" fill="#030303" />
+                              </svg>
+                            </span>
+                            <span class="shape-2">
+                              <svg width="67" height="41" viewBox="0 0 67 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M67 0.250122H0V44.0002L67 0.250122Z" fill="#C4EE18" />
+                              </svg>
+                            </span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="col-xxl-4 col-xl-3 col-lg-5">
+                  <div class="tp-hero-wd-right mb-30">
+                      <img class="mb-150" src="assets/img/hero/hero-3/shape.png" alt="">
+                      <div>
+                        <h4 class="tp-ff-teko fw-500 fs-35 mb-5">Awwwards</h4>
+                        <p class="fw-400 fs-20">Top Contributor since 2019 to current.</p>
+                      </div>
+                  </div>
+                </div>
+            </div>
           </div>
-
-        </div>
       </div>
-    </section>
-    <!-- IT Solution Hero Section - End
-    ================================================== -->
+      <div class="tp-hero-wd-bottom p-relative scale-up-img z-index-1 fix">
+          <div class="box tp-hero-wd-bottom-bg">
+            <img data-speed=".8" class="img-cover scale-up" src="assets/img/hero/hero-3/bg.jpg" alt="">
+          </div>
+          <div class="container">
+            <div class="row">
+                <div class="col-xxl-3 col-xl-4 col-lg-5 col-md-6 col-sm-8">
+                  <div class="tp-hero-wd-bottom-info tp-bg-common-black">
+                      <h5 class="tp-hero-wd-bottom-title tp-ff-teko fw-500 fs-25 tp-text-common-white lh-30 mb-140">Making a Brand on the <span class="tp-text-theme-primary">Digital Marketplace!</span></h5>
+                      <div>
+                        <span class="tp-ff-teko fs-25 tp-text-grey-2">See Our Work</span>
+                        <span class="tp-hero-wd-bottom-border">
+                            <svg height="6" viewBox="0 0 252 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M5 2.5L0 0.113249V5.88675L5 3.5V2.5ZM247 3.5L252 5.88675V0.113249L247 2.5V3.5ZM4.5 3.5H247.5V2.5H4.5V3.5Z" fill="white" fill-opacity="0.15" />
+                            </svg>
+                        </span>
+                        <div class="tp-footer-social">
+                            <ul>
+                              <li>
+                                  <a href="#">
+                                    <i class="fa-brands fa-dribbble"></i>
+                                    Dribbble
+                                  </a>
+                              </li>
+                              <li>
+                                  <a href="#">
+                                    <i class="fa-brands fa-behance"></i>
+                                    Behance
+                                  </a>
+                              </li>
+                              <li>
+                                  <a href="#">
+                                    <i class="fa-brands fa-pinterest"></i>
+                                    Pinterest
+                                  </a>
+                              </li>
+                              <li>
+                                  <a href="#">
+                                    <i class="fa-brands fa-dribbble"></i>
+                                    LinkedIn
+                                  </a>
+                              </li>
+                            </ul>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+      </div>
+    </div>
+    <!-- hero area end -->
     
 	<?php
 	}
