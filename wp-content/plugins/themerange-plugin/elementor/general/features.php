33587a2d2d3b7f91c97898f632cd2825a2fd3c26
Ahmad Madni <155092937+snankls@users.noreply.github.com>
2025-12-31 01:09:09 +0500

    feature update

--- wp-content/plugins/themerange-plugin/elementor/general/features.php
+++ wp-content/plugins/themerange-plugin/elementor/general/features.php
@@ -136,7 +136,7 @@ class TR_Elementor_Widget_Features extends TR_Elementor_Widget_Base{
         <div class="row">
           <div class="col-lg-12">
             <div class="text-center mb-60">
-              <h2 class="tp-ff-teko tp-text-perspective fw-600 fs-70 fs-sm-60 fs-xs-42 text-uppercase tp-text-common-white lh-1"><?php echo esc_attr($settings['title']); ?></h2>
+              <h2 class="tp-ff-teko tp-text-perspective fw-600 fs-70 fs-sm-60 fs-xs-42 text-uppercase tp-text-common-white lh-1"><?php echo wp_kses($settings['title'], true); ?></h2>
             </div>
           </div>
 
@@ -151,18 +151,18 @@ class TR_Elementor_Widget_Features extends TR_Elementor_Widget_Base{
               <span class="tp-skill-wd-icon mb-35">
                 <?php \Elementor\Icons_Manager::render_icon($item['icon']); ?>
               </span>
-              <h3 class="tp-ff-teko fw-600 fs-35 fs-lg-30 tp-text-theme-primary mb-20"><?php echo esc_attr($item['highlighted_title']); ?> <span class="tp-text-common-white"><?php echo esc_attr($item['normal_title']); ?></span></h3>
-              <p class="fs-18 tp-text-grey-2"><?php echo esc_attr($item['text']); ?></p>
+              <h3 class="tp-ff-teko fw-600 fs-35 fs-lg-30 tp-text-theme-primary mb-20"><?php echo wp_kses($item['highlighted_title'], true); ?> <span class="tp-text-common-white"><?php echo wp_kses($item['normal_title'], true); ?></span></h3>
+              <p class="fs-18 tp-text-grey-2"><?php echo wp_kses($item['text'], true); ?></p>
             </div>
           </div>
           <?php endforeach; ?>
           
           <div class="col-lg-12">
             <div class="tp-skill-wd-bottom text-center mt-35 tp_fade_anim" data-delay=".4" data-fade-from="bottom" data-ease="bounce">
-              <p class="tp-skill-wd-para fw-500 fs-18 tp-text-common-white"><?php echo esc_attr($settings['bottom_text']); ?>
+              <p class="tp-skill-wd-para fw-500 fs-18 tp-text-common-white"><?php echo wp_kses($settings['bottom_text'], true); ?>
                 <a <?php echo implode(' ', $link_attr); ?> class="ml-40 d-inline-block lh-0 tp-round-26 fs-15 text-uppercase ls-0 tp-btn-switch-animation tp-text-theme-primary tp-ff-heading fw-500">
                   <span class="d-flex align-items-center justify-content-center">
-                    <span class="btn-text"><?php echo esc_attr($settings['btn_name']); ?></span>
+                    <span class="btn-text"><?php echo wp_kses($settings['btn_name'], true); ?></span>
                     <span class="btn-icon">
                       <svg width="25" height="10" viewBox="0 0 25 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M18.675 9.91054L24.72 5.63362C24.806 5.56483 24.8766 5.47086 24.9255 5.36023C24.9744 5.2496 25 5.12579 25 5C25 4.87421 24.9744 4.7504 24.9255 4.63977C24.8766 4.52914 24.806 4.43518 24.72 4.36638L18.675 0.0894619C18.5572 0.0111909 18.4215 -0.0168364 18.2892 0.00979851C18.157 0.0364334 18.0358 0.116215 17.9446 0.236567C17.8535 0.356918 17.7977 0.510993 17.7859 0.674501C17.7742 0.838009 17.8072 1.00165 17.8798 1.13963L19.633 4.26665L0.598757 4.26665C0.439957 4.26665 0.287661 4.34391 0.175371 4.48144C0.0630817 4.61897 0 4.8055 0 5C0 5.1945 0.0630817 5.38103 0.175371 5.51856C0.287661 5.65609 0.439957 5.73335 0.598757 5.73335L19.633 5.73335L17.8798 8.86038C17.8072 8.99835 17.7742 9.16199 17.7859 9.3255C17.7977 9.48901 17.8535 9.64308 17.9446 9.76343C18.0358 9.88378 18.157 9.96357 18.2892 9.9902C18.4215 10.0168 18.5572 9.98881 18.675 9.91054Z" fill="currentColor" />
