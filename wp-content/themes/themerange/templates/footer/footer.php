f7af6e3698aab9dbf3f349c524e9383d6da28706
Ahmad Madni <155092937+snankls@users.noreply.github.com>
2025-12-31 14:34:04 +0500

    footer update

--- wp-content/themes/themerange/templates/footer/footer.php
+++ wp-content/themes/themerange/templates/footer/footer.php
@@ -1,150 +1,93 @@
 <?php $tr_theme_options = themerange_get_theme_options(); ?>
 
 <footer>
-
-  <!-- tp-footer area start -->
-  <div class="tp-footer-area tp-bg-common-black p-relative z-index-1 pt-105">
-      <span class="tp-footer-shape">
-        <svg width="402" height="339" viewBox="0 0 402 339" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <mask id="mask0_203_4" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="402" height="339">
-              <rect x="0.5" y="0.5" width="401" height="338" fill="#D9D9D9" stroke="black" />
-            </mask>
-            <g mask="url(#mask0_203_4)">
-              <circle cx="-4.5" cy="413.5" r="353.5" stroke="#0D0D0D" stroke-width="120" />
-            </g>
-        </svg>
-      </span>
-      <div class="tp-footer-top pb-30">
-        <div class="container">
-            <div class="row align-items-end">
-              <div class="col-lg-9">
-                  <div class="tp-footer-top-social-wrap mb-30">
-                    <span class="tp-footer-top-subtitle tp-text-theme-primary fw-500 fs-18 tp-ff-heading tp_fade_anim" data-delay=".3">Get’s Started a Projects? 
-                        <svg class="ml-20" width="74" height="12" viewBox="0 0 74 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M65.416 11.8926L73.62 6.76034C73.7367 6.67779 73.8325 6.56504 73.8989 6.43228C73.9652 6.29953 73.9999 6.15095 73.9999 6C73.9999 5.84905 73.9652 5.70048 73.8989 5.56772C73.8325 5.43496 73.7367 5.32221 73.62 5.23966L65.416 0.107354C65.2561 0.0134291 65.0719 -0.0202036 64.8925 0.0117582C64.713 0.04372 64.5485 0.139459 64.4248 0.28388C64.3011 0.428302 64.2254 0.613192 64.2094 0.809402C64.1935 1.00561 64.2383 1.20198 64.3369 1.36755L66.7161 5.11998L0.812592 5.11998C0.597076 5.11998 0.390396 5.21269 0.238007 5.37773C0.0856171 5.54277 0 5.7666 0 6C0 6.2334 0.0856171 6.45724 0.238007 6.62227C0.390396 6.78731 0.597076 6.88003 0.812592 6.88003L66.7161 6.88003L64.3369 10.6325C64.2383 10.798 64.1935 10.9944 64.2094 11.1906C64.2254 11.3868 64.3011 11.5717 64.4248 11.7161C64.5485 11.8605 64.713 11.9563 64.8925 11.9882C65.0719 12.0202 65.2561 11.9866 65.416 11.8926Z" fill="#C4EE18" />
-                        </svg>
-                    </span>
-                    <h2 class="tp-footer-top-title tp-text-common-white text-uppercase fw-500 rotate-text-anim"><a href="contact-light.html">Let’s Talk</a></h2>
-                    <div class="tp-footer-social tp_fade_anim" data-delay=".4" data-fade-from="bottom" data-ease="bounce">
-                        <ul>
-                          <li>
-                              <a href="#">
-                                <i class="fa-brands fa-dribbble"></i>
-                                Dribbble
-                              </a>
-                          </li>
-                          <li>
-                              <a href="#">
-                                <i class="fa-brands fa-behance"></i>
-                                Behance
-                              </a>
-                          </li>
-                          <li>
-                              <a href="#">
-                                <i class="fa-brands fa-pinterest"></i>
-                                Pinterest
-                              </a>
-                          </li>
-                          <li>
-                              <a href="#">
-                                <i class="fa-brands fa-dribbble"></i>
-                                LinkedIn
-                              </a>
-                          </li>
-                        </ul>
-                    </div>
-                  </div>
-              </div>
-              <div class="col-lg-3">
-                  <div class="tp-rounded-btn-wrap tp-footer-btn text-lg-end mb-40 tp_fade_anim" data-delay=".5" data-fade-from="top" data-ease="bounce">
-                    <div class="btn_wrapper d-inline-block">
-                        <a href="contact-light.html" class="tp-btn-rounded btn-item">
-                          <span class="d-block mb-10">
-                              <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3791 3.0269C14.6431 2.80336 18.8916 1.42595 21.9998 0C20.5732 3.10763 19.1953 7.35556 18.9723 10.6196L16.8276 6.04382L1.05193 21.82C0.936264 21.9354 0.779526 22.0001 0.616152 22C0.494263 22 0.375118 21.9638 0.273781 21.8961C0.172441 21.8284 0.0934544 21.7321 0.046814 21.6195C0.000171661 21.5069 -0.0120335 21.383 0.0117397 21.2634C0.035511 21.1439 0.0941944 21.034 0.18037 20.9478L15.956 5.17221L11.3791 3.0269Z" fill="currentColor" />
-                              </svg>
-                          </span>
-                          Start the Journey
-                          <i class="tp-btn-circle-dot"></i>
-                        </a>
-                    </div>
-                  </div>
-              </div>
-            </div>
-        </div>
-      </div>
-      <div class="tp-footer-main pt-45 pb-50">
-        <div class="container">
-            <div class="row">
-              <div class="col-lg-5 col-md-6">
-                  <div class="tp-footer-widget mb-45 tp_fade_anim" data-delay=".3">
-                    <div class="tp-footer-logo mb-25">
-                        <a href="index.html"><img data-width="150" src="assets/img/logo/logo-white.png" alt=""></a>
-                    </div>
-                    <p class="fw-500 fs-18 tp-text-grey-2 lh-28">We’re Global Digital <br>
-                        Agency Since-2018 to Provide Smart<br> Solutions.</p>
-                  </div>
-              </div>
-              <div class="col-lg-7">
-                  <div class="row">
-                    <div class="col-lg-7 col-md-6 col-sm-6">
-                        <div class="tp-footer-widget mb-60 tp_fade_anim" data-delay=".5">
-                          <h3 class="tp-footer-widget-title tp-ff-heading fs-25 mb-15 text-uppercase tp-text-common-white">New York</h3>
-                          <a class="fw-500 fs-18 tp-text-grey-2 lh-28 hover-text-white" href="#">Apple Inc.<br> One Apple Park Way Cupertino, CA <br>95014, United States</a>
+    <!-- tp-footer area start -->
+    <div class="tp-testimonial-wd-spacing">
+        <div class="tp-footer-area tp-bg-common-black-5 pt-155">
+            <div class="container-fluid container-1524">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="tp-footer-vp-widget mb-40">
+                            <div class="mb-15">
+                                <a class="tp-ff-inter fw-500 fs-24 lh-120-per ls-m-4 tp-text-grey-5 hover-text-white underline-white" href="tel:+923007101235">+92(300) 710-1235</a>
+                            </div>
+                            <div>
+                                <a class="tp-ff-inter fw-500 fs-42 lh-120-per ls-m-2 tp-text-grey-5 hover-text-white underline-white" href="mailto:info@themerange.net">info@themerange.net</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-5 col-md-6 col-sm-6">
-                        <div class="tp-footer-widget mb-60 tp_fade_anim" data-delay=".7">
-                          <h3 class="tp-footer-widget-title tp-ff-heading fs-25 mb-15 text-uppercase tp-text-common-white">London</h3>
-                          <a class="fw-500 fs-18 tp-text-grey-2 lh-28 hover-text-white" href="#">Belgrave House<br> 76 Buckingham Palace Road, London SW1W 9TQ, United Kingdom</a>
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="tp-footer-vp-widget ml-70 mb-40">
+                            <span class="tp-ff-inter fw-500 fs-16 ls-m-4 tp-text-grey-5 d-inline-block mb-20">Navigation</span>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contacts</a></li>
+                            </ul>
                         </div>
                     </div>
-                    <div class="col-lg-12">
-                        <div class="tp-footer-widget-form mb-60 tp_fade_anim" data-delay=".9">
-                          <h3 class="tp-footer-widget-title tp-ff-heading fs-25 mb-25 text-uppercase tp-text-common-white">Newsletter</h3>
-                          <form class="p-relative" action="#">
-                              <input class="tp-input" type="text" placeholder="Enter Email Address">
-                              <button class="tp-button" type="submit">
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M6.56328 13.2091V16.6876C6.56385 16.8058 6.60161 16.9208 6.6712 17.0164C6.74079 17.1119 6.83868 17.1832 6.95102 17.22C7.06335 17.2569 7.18443 17.2574 7.2971 17.2216C7.40976 17.1858 7.50831 17.1155 7.57878 17.0206L9.61353 14.2516L6.56328 13.2091ZM17.7645 0.104306C17.68 0.044121 17.5805 0.00848417 17.477 0.00133654C17.3735 -0.00581108 17.27 0.015809 17.178 0.0638058L0.303026 8.87631C0.205932 8.92762 0.125932 9.00617 0.0728436 9.1023C0.019755 9.19843 -0.00411253 9.30798 0.00417016 9.41748C0.0124529 9.52699 0.0525254 9.6317 0.11947 9.71875C0.186415 9.80581 0.277319 9.87143 0.381026 9.90756L5.07228 11.5111L15.063 2.96856L7.33203 12.2828L15.1943 14.9701C15.2723 14.9963 15.3551 15.0051 15.4369 14.996C15.5187 14.9868 15.5975 14.9599 15.6678 14.9171C15.7381 14.8743 15.7982 14.8167 15.8439 14.7482C15.8896 14.6798 15.9197 14.6021 15.9323 14.5208L17.9948 0.645806C18.01 0.543093 17.9966 0.438159 17.956 0.342598C17.9154 0.247038 17.8491 0.164569 17.7645 0.104306Z" fill="#C4EE18" />
-                                </svg>
-                              </button>
-                          </form>
+                    <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="tp-footer-vp-widget ml-20 mb-40">
+                            <span class="tp-ff-inter fw-500 fs-16 ls-m-4 tp-text-grey-5 d-inline-block mb-20">Social</span>
+                            <ul>
+                                <li>
+                                    <a href="#">Twitter
+                                        <svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M10 0L3.09778 2.94833L9.10222 7.45167L10 0ZM1 12L1.52 12.39L7.01 5.07L6.49 4.68L5.97 4.29L0.48 11.61L1 12Z" fill="currentColor" />
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">Instagram
+                                        <svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M10 0L3.09778 2.94833L9.10222 7.45167L10 0ZM1 12L1.52 12.39L7.01 5.07L6.49 4.68L5.97 4.29L0.48 11.61L1 12Z" fill="currentColor" />
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">LinkedIn
+                                        <svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M10 0L3.09778 2.94833L9.10222 7.45167L10 0ZM1 12L1.52 12.39L7.01 5.07L6.49 4.68L5.97 4.29L0.48 11.61L1 12Z" fill="currentColor" />
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">Dribble
+                                        <svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M10 0L3.09778 2.94833L9.10222 7.45167L10 0ZM1 12L1.52 12.39L7.01 5.07L6.49 4.68L5.97 4.29L0.48 11.61L1 12Z" fill="currentColor" />
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
-                  </div>
-              </div>
+                </div>
+                <div class="tp-footer-vp-bottom pb-15">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-7">
+                            <div class="tp-footer-copyright">
+                                <p class="tp-footer-it-copyright mb-10 tp-text-grey-5 tp-ff-inter">
+                                    <span>
+                                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M0.875 7C0.875 8.62445 1.52031 10.1824 2.66897 11.331C3.81763 12.4797 5.37555 13.125 7 13.125C8.62445 13.125 10.1824 12.4797 11.331 11.331C12.4797 10.1824 13.125 8.62445 13.125 7C13.125 5.37555 12.4797 3.81763 11.331 2.66897C10.1824 1.52031 8.62445 0.875 7 0.875C5.37555 0.875 3.81763 1.52031 2.66897 2.66897C1.52031 3.81763 0.875 5.37555 0.875 7ZM14 7C14 8.85652 13.2625 10.637 11.9497 11.9497C10.637 13.2625 8.85652 14 7 14C5.14348 14 3.36301 13.2625 2.05025 11.9497C0.737498 10.637 0 8.85652 0 7C0 5.14348 0.737498 3.36301 2.05025 2.05025C3.36301 0.737498 5.14348 0 7 0C8.85652 0 10.637 0.737498 11.9497 2.05025C13.2625 3.36301 14 5.14348 14 7ZM7.12775 4.368C6.06725 4.368 5.44162 5.173 5.44162 6.55725V7.48475C5.44162 8.85938 6.05675 9.639 7.12775 9.639C7.98438 9.639 8.56363 9.12625 8.64062 8.39825H9.77375V8.47962C9.68625 9.74662 8.589 10.6383 7.1225 10.6383C5.29288 10.6383 4.26213 9.46925 4.26213 7.48563V6.54675C4.26213 4.56838 5.313 3.3635 7.12337 3.3635C8.59425 3.3635 9.6915 4.28575 9.77375 5.614V5.691H8.64062C8.56363 4.92188 7.96863 4.368 7.12775 4.368Z" fill="#F3F1F2" />
+                                        </svg>
+                                    </span>
+                                    Copyright 2025 <a href="#" class="underline-white">ThemePure.</a> All Right Reserves. 
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-5">
+                            <div class="tp-footer-md-copyright-menu text-md-end mb-10">
+                                <a href="#" class="tp-text-grey-5 tp-ff-dm opacity-8 hover-text-white hover-opacity-1">Privacy Policy</a>
+                                <a href="#" class="tp-text-grey-5 tp-ff-dm opacity-8 hover-text-white hover-opacity-1">Terms of Use</a>
+                                <a href="#" class="tp-text-grey-5 tp-ff-dm opacity-8 hover-text-white hover-opacity-1">Site Map</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-      <div class="tp-footer-bottom">
-        <div class="container">
-            <div class="row">
-              <div class="col-lg-6">
-                  <div class="tp-footer-copyright">
-                    <p class="mb-0 tp-text-grey-2">
-                        <span>
-                          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M0.875 7C0.875 8.62445 1.52031 10.1824 2.66897 11.331C3.81763 12.4797 5.37555 13.125 7 13.125C8.62445 13.125 10.1824 12.4797 11.331 11.331C12.4797 10.1824 13.125 8.62445 13.125 7C13.125 5.37555 12.4797 3.81763 11.331 2.66897C10.1824 1.52031 8.62445 0.875 7 0.875C5.37555 0.875 3.81763 1.52031 2.66897 2.66897C1.52031 3.81763 0.875 5.37555 0.875 7ZM14 7C14 8.85652 13.2625 10.637 11.9497 11.9497C10.637 13.2625 8.85652 14 7 14C5.14348 14 3.36301 13.2625 2.05025 11.9497C0.737498 10.637 0 8.85652 0 7C0 5.14348 0.737498 3.36301 2.05025 2.05025C3.36301 0.737498 5.14348 0 7 0C8.85652 0 10.637 0.737498 11.9497 2.05025C13.2625 3.36301 14 5.14348 14 7ZM7.12775 4.368C6.06725 4.368 5.44162 5.173 5.44162 6.55725V7.48475C5.44162 8.85938 6.05675 9.639 7.12775 9.639C7.98438 9.639 8.56363 9.12625 8.64062 8.39825H9.77375V8.47962C9.68625 9.74662 8.589 10.6383 7.1225 10.6383C5.29288 10.6383 4.26213 9.46925 4.26213 7.48563V6.54675C4.26213 4.56838 5.313 3.3635 7.12337 3.3635C8.59425 3.3635 9.6915 4.28575 9.77375 5.614V5.691H8.64062C8.56363 4.92188 7.96863 4.368 7.12775 4.368Z" fill="#999999" />
-                          </svg>
-                        </span>
-                        Copyright 2025 <a class="tp-text-common-white hover-text-primary" href="#">ThemePure.</a> All Right Reserves. 
-                    </p>
-                  </div>
-              </div>
-              <div class="col-lg-6">
-                  <div class="tp-footer-menu">
-                    <ul>
-                        <li><a href="#">Career</a></li>
-                        <li><a href="portfolio-col-4-light.html">Our Work</a></li>
-                        <li><a href="contact-light.html">Contact</a></li>
-                    </ul>
-                  </div>
-              </div>
-            </div>
-        </div>
-      </div>
-  </div>
-  <!-- tp-footer area end -->
-
+    </div>
+    <!-- tp-footer area end -->
 </footer>
