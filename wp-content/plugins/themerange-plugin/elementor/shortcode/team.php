586dae4dbe23c541ee6a32df573b3a3bf433c19b
Ahmad Madni <155092937+snankls@users.noreply.github.com>
2026-03-25 12:06:23 +0500

    team update

--- wp-content/plugins/themerange-plugin/elementor/shortcode/team.php
+++ wp-content/plugins/themerange-plugin/elementor/shortcode/team.php
@@ -103,7 +103,9 @@ class TR_Elementor_Widget_Team extends TR_Elementor_Widget_Base{
 							if($i % 2) $speed = '.9'; else $speed = '-.9'; ?>
 							<div class="col-lg-3 col-md-6">
 								<div class="tp-team-it-item mb-90" data-speed="<?php echo $speed; ?>">
-									<img class="myimg" src="<?php echo wp_get_attachment_url($item['image']['id']); ?>" alt="<?php echo esc_attr($item['title']); ?>">
+									<div class="tp-team-it-thumb">
+										<img class="myimg" src="<?php echo wp_get_attachment_url($item['image']['id']); ?>" alt="<?php echo esc_attr($item['title']); ?>">
+									</div>
 									
 									<div class="tp-team-it-content p-relative">
 										<h3 class="fw-600 fs-28 fs-lg-25 ls-m-4 tp-ff-inter tp-text-common-black-1"><?php echo wp_kses($item['title'], true); ?></h3>
